925191f111f183a8b690330c71a4a30227ba7740
ashokranera <ashokranera>
2020-10-08 06:41:33 +0530

    Uploading Data - Firebase to Firestore

--- app/src/main/java/com/example/firebasefirestore/MainActivity.kt
+++ app/src/main/java/com/example/firebasefirestore/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.firebasefirestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val personCollectionRef = Firebase.firestore.collection("persons")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_btnUploadData.setOnClickListener {
+            val firstName = edt_FirstName.text.toString()
+            val lastName = edt_LastName.text.toString()
+            val age = edt_Age.text.toString().toInt()
+            val person = Person(firstName, lastName, age)
+            savePerson(person)
+        }
+    }
+
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
+
+        try {
+            personCollectionRef.add(person).await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Successfully saved Data", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
