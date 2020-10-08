cc9241318c1c9b7d0076321e4de8ee15812c9c01
ashokranera <ashokranera>
2020-10-08 08:27:30 +0530

    Retrieving Data

--- app/src/main/java/com/example/firebasefirestore/MainActivity.kt
+++ app/src/main/java/com/example/firebasefirestore/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -20,14 +21,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_btnUploadData.setOnClickListener {
+        btn_UploadData.setOnClickListener {
             val firstName = edt_FirstName.text.toString()
             val lastName = edt_LastName.text.toString()
             val age = edt_Age.text.toString().toInt()
             val person = Person(firstName, lastName, age)
             savePerson(person)
         }
+
+        btn_RetrieveData.setOnClickListener {
+            retrievePersons()
+        }
+
     }
+
+    private fun retrievePersons() = CoroutineScope(Dispatchers.IO).launch {
+
+        try {
+            val querySnapshot = personCollectionRef.get().await()
+            val sb = StringBuilder()
+            for (document in querySnapshot.documents) {
+                val person = document.toObject<Person>()
+                sb.append("$person\n")
+            }
+            withContext(Dispatchers.Main) {
+                txt_Persons.text = sb.toString()
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
 
     private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
 
